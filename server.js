8d3dab168e21df8766152268345c03a442160971
Olubukola Abiona <bukyabiona@gmail.com>
2023-01-25 17:51:22 -0400

    created notes.json file and successfully returned data back to the browser from server

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
-const port = process.nextTick.PORT || 3001;
-const fs = require('fs'); 
+const PORT = process.env.PORT|| 3001;
+// const fs = require('fs'); 
 const path = require('path');
-
+const { v4: uuidv4 } = require('uuid');
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require('./helpers/fsUtils');
+  
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,23 +17,50 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+//route returning home page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+
 //route returning notes page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 })
 
-//app.get()
+app.get('/api/notes', (req, res) => {
+    // res.sendFile(path.join(__dirname, '/public/notes.html'));
+    readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+})
 
-//route returning index page
+
+
+//route returning any other page
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    const { title, text } = req.body;
+
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        note_id: uuidv4()
+      };
+  
+      readAndAppend(newNote, './db/notes.json');
+      res.json(`New note added successfully 🚀`);
+    } else {
+      res.error('Error in adding note');
+    }
 })
 
 
 
 
 
-
-app.listen(port, () => {
-    console.log(`Application is running and listening at ${port}`);
+app.listen(PORT, () => {
+    console.log(`Application is running and listening at ${PORT}`);
 })
