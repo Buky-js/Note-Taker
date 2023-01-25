8b5c1a364225617d1571be0bd9be2f5319710a7f
Olubukola Abiona <bukyabiona@gmail.com>
2023-01-25 18:18:56 -0400

    modified the index.js file to display existing note in right column when selected

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -57,7 +57,7 @@ const deleteNote = (id) =>
 const renderActiveNote = () => {
   hide(saveNoteBtn);
 
-  if (activeNote.id) {
+  if (activeNote.note_id) {
     noteTitle.setAttribute('readonly', true);
     noteText.setAttribute('readonly', true);
     noteTitle.value = activeNote.title;
